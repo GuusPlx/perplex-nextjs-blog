f61612ba608f175cfffae0cc5b59efd1df57d6d8
Guus Noij <guus.noij@perplex.nl>
2022-08-16 15:02:27 +0200

    added umsendpoint env var

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ type mockUmsResponse = {
 }
 
 async function registerPageView(ip: string): Promise<mockUmsResponse> {
-  const data = fetch('https://ums-mock.herokuapp.com?' + new URLSearchParams({
+  const data = fetch(process.env.UMS_ENDPOINT + '?' + new URLSearchParams({
     ip: ip ?? 'unknown ip',
   }))
     .then(response => response.json())
@@ -16,19 +16,21 @@ async function registerPageView(ip: string): Promise<mockUmsResponse> {
 export default async function middleware(req: NextRequest) {
   const { nextUrl: url } = req;
 
-  try {
-    console.log(req);
-    const data = await registerPageView(req.ip);
-    const visitorId = data.visitorId;
-
-    console.log(visitorId);
+  if(process.env.UMS_ENDPOINT) {
+    try {
+      console.log(req);
+      const data = await registerPageView(req.ip);
+      const visitorId = data.visitorId;
   
-    if(visitorId === '12345678') {
-      req.nextUrl.pathname = '/personalisation'
-      return NextResponse.rewrite(req.nextUrl)
+      console.log(visitorId);
+    
+      if(visitorId === '12345678') {
+        req.nextUrl.pathname = '/personalisation'
+        return NextResponse.rewrite(req.nextUrl)
+      }
+    } catch (e) {
+      console.log(e);
     }
-  } catch (e) {
-    console.log(e);
   }
 
   return NextResponse.rewrite(url)
